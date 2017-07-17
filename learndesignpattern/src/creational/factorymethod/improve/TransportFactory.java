59e6405b79459f24c6d9c3981e2c59c2a8db19d4
TuanLQ <tuan.lq@eprtech.com>
2017-07-17 22:39:20 +0700

    update

---
+++ learndesignpattern/src/creational/factorymethod/improve/TransportFactory.java
@@ -0,0 +1,8 @@
+package creational.factorymethod.improve;
+
+/**
+ * Created by TuanLQ on 5/29/17.
+ */
+public abstract class TransportFactory {
+    abstract Transport create();
+}
---
+++ learnjavase/src/javacollections/synchronize/SynchronizeDemo.java
@@ -0,0 +1,7 @@
+package javacollections.synchronize;
+
+/**
+ * Created by TuanLQ on 18/06/2017.
+ */
+public class SynchronizeDemo {
+}
